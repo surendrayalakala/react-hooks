366330637f3ee63fa5ea419771ad82ea4530680e
Surendra Yalakala <v-syalak@microsoft.com>
2022-01-06 12:16:54 +0530

    state with prev state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ClassCounter from './components/state/ClassCounter';
 import HookCounter from './components/state/HookCounter';
+import HookCounterTwo from './components/state/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <ClassCounter />
-      <HookCounter />
+      {/* <ClassCounter />
+      <HookCounter /> */}
+      <HookCounterTwo />
     </div>
   );
 }
